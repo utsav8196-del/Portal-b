06e331ea5707e19120a21f9202ec2ae32905bb27
utsav8196-del <utsav8196@gmail.com>
2026-05-13 16:06:14 +0530

    asasasas

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose.connection.once('open', async () => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
